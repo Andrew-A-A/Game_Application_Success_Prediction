cd2c827b67c463b1c3fe5101b5c7601b008c8292
Andrew Adel <20201700157@cis.asu.edu.eg>
2023-04-14 09:59:49 +0200

    1.Fill missing values 2.Drop unimportant columns 3.Convert datatype of several columns 4.Encode categorical data

--- Preprocessing.py
+++ Preprocessing.py
@@ -1,19 +1,56 @@
 import pandas as pd
-import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder
+import numpy as np
+
+
+def feature_encoder(x, cols):
+    for c in cols:
+        lbl = LabelEncoder()
+        lbl.fit(list(x[c].values))
+        x[c] = lbl.transform(list(x[c].values))
+    return x
+
+
 df = pd.read_csv("games-regression-dataset.csv")
 
-# Print number of missing values in each column
-print(df.isnull().sum())
+# Fill missing values in "In-app purchase" column with zero
+df["In-app Purchases"] = df["In-app Purchases"].fillna(0)
 
-# Number of duplicated data
-print(f"Number of duplicated data {df.duplicated().sum()}")
+# Drop subtitle column
+df = df.drop('Subtitle', axis=1)
 
-# Dataframe size
-originalSize = df.shape[0]
-print(f"The total size is {originalSize}")
+# Calculate the mode of column 'Languages'
+mean = df['Languages'].mode().iloc[0]
+
+# Fill missing values in column 'Languages' with the mode
+df['Languages'] = df['Languages'].fillna(mean)
 
 # Drop the duplicates
 df = df.drop_duplicates()
-print("Duplicated data dropped !")
-print(f"Number of dropped columns {originalSize - df.shape[0]}")
+df = df.convert_dtypes()
 
+# Remove the '+' sign from the 'Age rating' column
+df['Age Rating'] = df['Age Rating'].str.replace('+', '', regex=False)
+
+# Convert the 'Age rating' column to an integer data type
+df['Age Rating'] = df['Age Rating'].astype(int)
+
+# Encode categorical columns
+categorical_columns = ('URL', 'Name', 'Icon URL', 'Description', 'Developer',
+                       'Languages', 'Primary Genre', 'Genres')
+feature_encoder(df, categorical_columns)
+
+# Extract features from date columns (Date => Day, Month, Year)
+df['Original Release Date'] = pd.to_datetime(df['Original Release Date'], dayfirst=True)
+df['Current Version Release Date'] = pd.to_datetime(df['Current Version Release Date'], dayfirst=True).dt.date
+
+
+print(df.dtypes)
+
+
+def feature_scaling(x, a, b):
+    x = np.array(x)
+    normalized_x = np.zeros((x.shape[0], x.shape[1]))
+    for i in range(x.shape[1]):
+        normalized_x[:, i] = ((x[:, i] - min(x[:, i])) / (max(x[:, i]) - min(x[:, i]))) * (b - a) + a
+    return normalized_x
