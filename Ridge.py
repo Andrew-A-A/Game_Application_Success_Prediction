2382ca7832dcb85d63d83cebabc133f18f041524
Monica Sameh Salib <monica.sameh.salib@gmail.com>
2023-04-19 03:55:48 +0200

    Implement Ridge Model

---
+++ Ridge.py
@@ -0,0 +1,16 @@
+from App import *
+from sklearn.linear_model import RidgeCV
+from sklearn.model_selection import KFold
+
+#Ridge Regression Model
+kfold=KFold(n_splits=5)
+ridgeReg = RidgeCV(alphas = [0.0001, 0.001,0.01, 0.1, 1, 10],cv=kfold)
+ridgeReg.fit(x_train,y_train)
+
+#train and test scorefor ridge regression
+train_score_ridge = ridgeReg.score(x_train, y_train)
+test_score_ridge = ridgeReg.score(x_test, y_test)
+
+print("\nRidge Model............................................\n")
+print("The train score for ridge model is {}".format(train_score_ridge))
+print("The test score for ridge model is {}".format(test_score_ridge))
