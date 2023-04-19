21bd137f26d8f9a5e0174d35f34ce514b845c4aa
MarizSedhom <marizsedhom@gmail.com>
2023-04-19 03:44:39 +0200

    Implement Lasso Model

---
+++ Lasso.py
@@ -0,0 +1,14 @@
+#Using the linear CV model
+from sklearn.linear_model import LassoCV
+import App
+from sklearn.model_selection import KFold
+
+#Lasso Cross validation
+k_folds = KFold(n_splits = 5)
+lasso_cv = LassoCV(alphas = [0.0001, 0.001,0.01, 0.1, 1, 10], cv=k_folds, random_state=0).fit(App.x_train, App.y_train)
+
+
+#score
+print("\nLasso Model............................................\n")
+print("The train score for lasso model is",lasso_cv.score(App.x_train, App.y_train))
+print("The test score for lasso model is",lasso_cv.score(App.x_test, App.y_test))
