4eff1639becbf01c87060f1a4024489071b911c6
Andrew Adel <20201700157@cis.asu.edu.eg>
2023-04-19 11:05:37 +0200

    Add comments to Preprocessing.py and App.py

--- App.py
+++ App.py
@@ -1,6 +1,5 @@
 from sklearn.model_selection import train_test_split
 from Preprocessing import *
-
 # Load the csv file
 df = pd.read_csv("games-regression-dataset.csv")
 
@@ -33,23 +32,31 @@ feature_encoder(df, categorical_columns)
 # calculate the sum of 'In-app Purchases'
 df['In-app Purchases'] = calc_sum_of_list(df['In-app Purchases'])
 
+# Split "Original release date" and "Current version date" into three columns
+# (Original release date_day, Original release date_month, Original release date_year)
 df = explode_date(df, 'Original Release Date')
 
+# (Current Version Release Date_day, Current Version Release Date_month, Current Version Release Date_year)
 df = explode_date(df, 'Current Version Release Date')
 
+# Apply hot one encoding for the "Genres" column
 df = hot_one_encode(df, 'Genres')
 
+# Split data frame to X and Y
 Y = df['Average_User_Rating']
 X = df.drop('Average_User_Rating', axis=1)
 
-# Split the data to training and testing sets
+# Split the X and the Y to training and testing sets
 x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.20, shuffle=False, random_state=0)
 
+# Feature selection
 df = wrapper_feature_selection(df, x_train, y_train, x_test, y_test)
 
 # Drop the duplicates
 df = df.drop_duplicates()
 
-df = outlier_IQR_replace(df)
+# Replace outliers
+df = outlier_iqr_replace(df)
 
+# Apply feature scaling
 df = feature_scale(df)
